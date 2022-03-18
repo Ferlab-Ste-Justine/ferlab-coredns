6f759ed30e6028d6099a225888480dcc8c4c8451
Eric Vallee <eric_vallee@webificservices.com>
2022-03-18 16:31:16 -0400

    fix: INFRA-6 Pipeline fix

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
-git clone https://github.com/coredns/coredns.git
-(cd coredns; git checkout v1.9.1)
-cp plugin.cfg coredns/
-(cd coredns; make)
+git clone https://github.com/coredns/coredns.git coredns-build
+(cd coredns-build; git checkout v1.9.1)
+cp plugin.cfg coredns-build/
+(cd coredns-build; make)
+cp coredns-build/coredns .
+mkdir linux-arm64
+cp coredns linux-arm64/
+zip -r linux-arm64.zip linux-arm64
+rm -fr coredns-build
+rm coredns
